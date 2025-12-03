d1f7bebae305af3713c25d470920e32453541ad0
Bryce Peters <brycepeters04@gmail.com>
2025-12-02 19:08:59 -0700

    noconfirm

--- archtimize.sh
+++ archtimize.sh
@@ -23,7 +23,7 @@ echo "==> Installing CachyOS repositories..."
 curl -L https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
 tar xvf cachyos-repo.tar.xz
 cd cachyos-repo
-sudo ./cachyos-repo.sh --noconfirm
+sudo ./cachyos-repo.sh
 cd ..
 
 echo "==> Syncing pacman..."
