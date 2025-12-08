e8f9b589836319739c66058d58f3811ff734854a
Bryce Peters <brycepeters04@gmail.com>
2025-12-07 19:20:09 -0700

    cleanup the cleanup

--- cleanup/start_cleanup.sh
+++ cleanup/start_cleanup.sh
@@ -2,14 +2,11 @@
 set -Eeuo pipefail
 
 # move cleanup script
-echo "1"
 mv /usr/local/bin/archtimize/cleanup/archtimize-cleanup.sh /usr/local/bin/archtimize-cleanup.sh
 chmod +x /usr/local/bin/archtimize-cleanup.sh
 
-echo "1"
 # move cleanup service
 mv /usr/local/bin/archtimize/cleanup/archtimize-cleanup.service /etc/systemd/system/archtimize-cleanup.service
 
-echo "1"
 systemctl daemon-reload
 systemctl enable archtimize-cleanup.service
